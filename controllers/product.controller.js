239556eea2dc3cb3dea031ff0b8bc655359069e3
Hoa Vy <vycoi1511@gmail.com>
2021-07-11 04:02:51 -0700

    Add product pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,17 @@ var Product = require('../models/product.model');
 
 module.exports.index = async (req, res) => {
     var products = await Product.find();
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 8;
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+    var totalPage = parseInt(products.length/8);
+    if (page % 8 !== 0) ++totalPage;        // 8 products per page
     res.render('products/index',{
-        products: products
+        products: products.slice(start, end),
+        page: page,
+        priviousPage: page - 1,
+        nextPage: page + 1,
+        totalPage: totalPage
     });
 }
